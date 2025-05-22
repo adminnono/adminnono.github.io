d1467f9451c032ae829c65d3683847551440c488
arnaud <arnaud.dujardin76@gmail.com>
2025-05-22 10:30:50 +0200

    Ajout projet javascript sidebar

---
+++ sidebar/index.js
@@ -0,0 +1,10 @@
+const sidebar = document.getElementById("side-bar");
+const content = document.querySelector(".content");
+
+btn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
+
+content.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
