b38ba31c532cbb40d875b8d7cfaad9a55cc277d7
arnaud <arnaud.dujardin76@gmail.com>
2025-06-10 09:25:43 +0200

    Ajout projet todo-list

---
+++ todo-list/index.js
@@ -0,0 +1,15 @@
+const form = document.querySelector("form");
+const list = document.getElementById("list");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  list.innerHTML += `<li>${item.value}</li>`;
+  item.value = "";
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+  }
+});
