8061f6d93868070ec0c4fb250a13154e4f7af43c
arnaud <arnaud.dujardin76@gmail.com>
2025-06-11 10:13:12 +0200

    Ajout compte à rebours

--- color-generator/index.js
+++ color-generator/index.js
@@ -1,12 +1,9 @@
-function getColor() {
+const getColor = () => {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
-
   const color = `rgb(${r}, ${g}, ${b})`;
-
   document.body.style.background = color;
   document.body.innerHTML = `<h2>${color}</h2>`;
-}
-
+};
 setInterval(getColor, 1000);
