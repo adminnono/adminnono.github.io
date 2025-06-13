ddae9c152204def96eaa2e71bdabd38edf11668f
arnaud <arnaud.dujardin76@gmail.com>
2025-06-13 10:55:33 +0200

    Ajout text calculator javascript

--- compte-a-rebours/index.js
+++ compte-a-rebours/index.js
@@ -1,10 +1,11 @@
 let totalSeconds;
 let interval;
+
 const countDown = () => {
   const minutes = Math.floor(totalSeconds / 60);
   const seconds = totalSeconds % 60;
   const sec = seconds < 10 ? "0" + seconds : seconds;
-  countdownDisplay.textContent = `${minutes} : ${sec}`;
+  countdownDisplay.textContent = `${minutes}:${sec}`;
   if (totalSeconds > 0) {
     totalSeconds--;
   } else {
@@ -16,7 +17,7 @@ const countDown = () => {
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (isNaN(choice.value)) {
-    alert("Je vais me facher");
+    alert("Rentre un nombre sinon je vais me fâcher");
   } else {
     totalSeconds = choice.value * 60;
     choice.value = "";
