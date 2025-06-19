f2076c1dc36a1e853839d107097e2c39d0a6eef8
arnaud <arnaud.dujardin76@gmail.com>
2025-06-19 10:00:16 +0200

    Ajout fetch joke app

---
+++ joke-app/index.js
@@ -0,0 +1,17 @@
+const header = document.getElementById("header");
+const text = document.getElementById("content");
+
+function getJoke() {
+  fetch("https://api.blablagues.net/?rub=blagues")
+    .then((res) => res.json())
+    .then((data) => {
+      const content = data.data.content;
+      header.textContent = content.text_head;
+      text.textContent =
+        content.text !== "" ? content.text : content.text_hidden;
+    });
+}
+
+getJoke();
+
+document.body.addEventListener("click", getJoke);
