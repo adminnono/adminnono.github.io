bf4adf51916b7021403ea71debd4d05532d75212
arnaud <arnaud.dujardin76@gmail.com>
2025-06-24 10:53:22 +0200

    Ajout fonction fetch

--- country-app/index.js
+++ country-app/index.js
@@ -1,13 +1,19 @@
-// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
-
-// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
+let countriesData = [];
+const fetchCountries = async () => {
+  await fetch(
+    "https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags"
+  )
+    .then((res) => res.json())
+    .then((data) => (countriesData = data));
+  console.log(countriesData);
+};
+window.addEventListener("load", fetchCountries);
 
 // 3 - Passer les données à une variable
 
 // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 
 // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-coutry.name.includes(inputSearch.value);
 
 // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
 
