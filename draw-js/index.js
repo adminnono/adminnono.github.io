68d5ce5584e1c04acb7d904dff6581bd159038e0
arnaud <arnaud.dujardin76@gmail.com>
2025-07-03 11:09:30 +0200

    logique dessin

--- draw-js/index.js
+++ draw-js/index.js
@@ -1,4 +1,34 @@
+const canvas = document.getElementById("art");
+const ctx = canvas.getContext("2d");
 
+const getMousePos = (e) => {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top,
+  };
+};
 
+const mouseMove = (e) => {
+  const mousePos = getMousePos(e);
+  ctx.lineTo(mousePos.x, mousePos.y);
+  ctx.stroke();
+  ctx.strokeStyle = "salmon";
+  ctx.lineWidth = 10;
+};
 
+canvas.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+  const mousePos = getMousePos(e);
+  ctx.beginPath();
+  ctx.moveTo(mousePos.x, mousePos.y);
 
+  canvas.addEventListener("mousemove", mouseMove);
+  canvas.addEventListener("mouseup", () => {
+    canvas.removeEventListener("mousemove", mouseMove);
+  });
+});
+
+reset.addEventListener("click", () => {
+  ctx.clearRect(0, 0, 800, canvas.width, canvas.height);
+});
